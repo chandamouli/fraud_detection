65e8836cec63786526c30b4b18438c13c063ca4c
chandramouli <contactkcm@gmail.com>
2021-12-23 21:32:26 +0530

    added new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("Adding all ml related files here")
+
+def model1():
+    print("adding code for model1")
+
+
+def model2():
+    print("adding code for model2")
+
+
+def model3():
+    print("adding code for model3")
